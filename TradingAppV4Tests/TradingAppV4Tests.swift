32a9c346b262e97b60b1c244577f3f1d50d0d63d
Allun <ocarina51006@gmail.com>
2024-10-20 21:27:09 +0800

    Initial Commit

---
+++ TradingAppV4Tests/TradingAppV4Tests.swift
@@ -0,0 +1,17 @@
+//
+//  TradingAppV4Tests.swift
+//  TradingAppV4Tests
+//
+//  Created by Aaron Lo on 2024/10/20.
+//
+
+import Testing
+@testable import TradingAppV4
+
+struct TradingAppV4Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
